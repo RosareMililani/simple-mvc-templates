fdbfe759358a23f42b2c4bea4cbc2fbc9521c1f3
RosareMililani <mr7721@rit.edu>
2020-10-15 19:30:55 -0400

    finished in-class demo

--- server/app.js
+++ server/app.js
@@ -7,11 +7,13 @@ const bodyParser = require('body-parser');
 
 const router = require('./router.js');
 
+const expressHandlebars = require('express-handlebars');
+
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const app = express();
 
-app.use('/assets', express.static(path.resolve(`${__dirname}/../client/`)));
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 
 app.use(compression());
@@ -19,6 +21,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cookieParser());
+
+// load the handlebars engine
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: '' // NOT using a main.handlebars layout file, which is kind of a "super template"
+}));
+
+app.set('view engine', 'handlebars'); // use the handlebars engine
+app.set('views', `${__dirname}/../views`); // tell express where the .handlebars files are
 
 router(app);
 
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,27 +3,45 @@
 let name = 'unknown';
 
 const hostIndex = (req, res) => {
-
+  res.render('index', { // `res.render()` is NEW for us - express knows to look for the index.handlebars file
+    title: "Home Page", 		// the data we want to pass into the template is in an object - in this case `title: "Home"`
+    pageName: "HomePage that exists",
+    userName: name,
+  });
 };
 
 const hostPage1 = (req, res) => {
-
+  res.render('page1', {
+    title: "Page1",
+  })
 };
 
 const hostPage2 = (req, res) => {
-
+  res.render('page2');
 };
 
 const getName = (req, res) => {
-
+  res.json({ name });
 };
 
 const setName = (req, res) => {
+  // handle POST request
+  // same as our previous demo
+  if (!req.body.firstname || !req.body.lastname) {
+    return res.status(400).json({
+      error: 'firstname and lastname are both required',
+      id: 'badRequest',
+    });
+  }
 
+  name = `${req.body.firstname} ${req.body.lastname}`;
+  return res.json({ name });
 };
 
 const notFound = (req, res) => {
-
+  res.status(404).render('notFound', {
+    page: req.url,
+  });
 };
 
 module.exports = {
